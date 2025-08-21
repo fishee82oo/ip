753d4570a576300b45af0d167bfa9b34a61ecc96
Reineil <fish82ee@gmail.com>
2025-08-21 22:50:37 +0800

    Level 0: Rename, Greet and Exit

---
+++ src/main/java/Fish.java
@@ -0,0 +1,18 @@
+public class Fish {
+    public static void main(String[] args) {
+        String logo =
+                "      __><(((º>    \n" +
+                        "     /             \n" +
+                        "~~~<'               \n" +
+                        "     \\__           \n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    public static void greet () {
+        System.out.println("Greetings from Fish~");
+    }
+
+    public static void exit () {
+        System.out.println("Goodbye Au Revoir~");
+    }
+}
