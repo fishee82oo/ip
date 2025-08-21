5e40402f96dbee2ac03bd3a640c31a75d9f186fc
Reineil <fish82ee@gmail.com>
2025-08-22 00:30:25 +0800

    Add, List

--- src/main/java/Fish.java
+++ src/main/java/Fish.java
@@ -6,13 +6,23 @@ public class Fish {
 
         Scanner sc = new Scanner(System.in);
 
-        // keep reading input until "bye"
+        String tasks = new String[100];
+        int idx = 0;
+
         while (true) {
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
                 exit();
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < idx; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else if (!input.isEmpty()) {
+                tasks[idx] = input;
+                idx++;
+                System.out.println("added: " + input);
             }
 
             System.out.println(input);
